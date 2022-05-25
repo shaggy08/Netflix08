c6f21c279891e9fdc46270056792df8a8ba7edc0
sahil <maliksahil2000@gmail.com>
2022-05-25 11:40:57 +0530

    banner video

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
- import { ArrowDropDown, Notifications, Search } from "@material-ui/icons";
+import { ArrowDropDown, Notifications, Search } from "@material-ui/icons";
 import "./Nav.css";
 
 function Nav() {
@@ -23,53 +23,56 @@ function Nav() {
   return (
     <div className={`nav ${show && "nav_black"}`}>
       <div className="container">
-
-      <div className="left">
-      <img
-        className="nav_logo"
-        src="http://www.freepnglogos.com/uploads/netflix-logo-0.png"
-        alt="NETFLIX"
-      />
-      <ul className="left_list" >
-          <li>
-              
-          </li>
-         <li className="li_lest" >Home</li> 
-         <li className="li_lest">Series</li> 
-         <li className="li_lest">Movies</li> 
-         <li className="li_lest">New and Popular</li> 
-         <li className="li_lest">My List </li>
-
-
-      </ul>
-        {/* <span>Home</span>
+        <div className="left">
+          <img
+            className="nav_logo"
+            src="http://www.freepnglogos.com/uploads/netflix-logo-0.png"
+            alt="NETFLIX"
+          />
+          <ul className="left_list">
+            <li className="li_lest">Home</li>
+            <li className="li_lest">Series</li>
+            <li className="li_lest">Movies</li>
+            <li className="li_lest">New and Popular</li>
+            <li className="li_lest">My List </li>
+          </ul>
+          {/* <span>Home</span>
         <span >Series</span>
         <span >Movies</span>
         <span>New and Popular</span>
           <span>My List</span> */}
-      </div>
-      <div className="right">
-          <ul>
-              <li className="li_right" > <Search /> </li>
-              <li className="li_right" > Children</li>
-              <li className="li_right" > <Notifications /> </li>
-              <li className="li_right" >   <img
-        className="nav_avatar"
-        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
-        alt="AVATAR"
-      /></li>
+        </div>
+        <div className="right">
+          <ul className="right_list">
+            <li className="li_right">
+              {" "}
+              <Search />{" "}
+            </li>
+            <li className="li_right child"> Children</li>
+            <li className="li_right">
+              {" "}
+              <Notifications />{" "}
+            </li>
+            <li className="li_right avt ">
+              {" "}
+              <img
+                className="nav_avatar"
+                src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+                alt="AVATAR"
+              />
+            </li>
           </ul>
-      {/* <Search  />
+          {/* <Search  />
       <span id="child">Children</span>
       <Notifications className="icon" /> */}
-      {/* <img
+          {/* <img
         className="nav_avatar"
         src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
         alt="AVATAR"
       /> */}
-      <div className="profile">
-            <ArrowDropDown className="icon" />
-            <div className="options">
+          <div className="profile arrow">
+            <ArrowDropDown className="icon arrow" />
+            <div className="options ">
               <span>profile 1</span>
               <span>profile 2</span>
               <span>profile 3</span>
@@ -77,11 +80,11 @@ function Nav() {
               <span>Settings1</span>
               <span>Settings2</span>
               <hr></hr>
-              <span >Logout</span>
+              <span>Logout</span>
             </div>
           </div>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,118 +1,105 @@
-
-
-import React, { useEffect, useState } from 'react';
-import './Row.css';
-import axios from '../axios';
-import YouTube ,{YouTubeProps}from 'react-youtube';
-import movieTrailer from 'movie-trailer'
-import getYoutubeId from 'get-youtube-id';
+import React, { useEffect, useState } from "react";
+import "./Row.css";
+import axios from "../axios";
+import YouTube, { YouTubeProps } from "react-youtube";
+import movieTrailer from "movie-trailer";
+import getYoutubeId from "get-youtube-id";
 import ReactPlayer from "react-player";
+import {
+  PlayArrow,
+  Add,
+  ThumbUpAltOutlined,
+  ThumbDownOutlined,
+} from "@material-ui/icons";
 
 const baseUrl = "https://image.tmdb.org/t/p/original/";
 
-function Row({ title , fetchUrl, isLargeRow }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
-  
+  const [isHovered, setIsHovered] = useState(false);
   const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
-    async function fetchData(){
-     const request = await axios.get(fetchUrl);
-     setMovies(request.data.results);
-     return request;
-    } fetchData();
-    
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
   }, [fetchUrl]);
- 
-   const opts = {
-     height: "390",
-     width: "100%",
-     playerVars: {
-      'autoplay': 1,
-      'controls': 0,
-      'origin': 'http://localhost:3000'
-     },
-   }
-     
 
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+      controls: 0,
+      origin: "http://localhost:3000",
+    },
+  };
 
-   const handleClick = (movie) => {
-     if(trailerUrl!==""){
-       setTrailerUrl("");
-     }
-   
-else{
-  var trail= movieTrailer(movie?.name || movie?.title )
- // console.log(movie.name);
-  trail.then(result => {
-    //console.log(result);
-  var youtubeId= getYoutubeId(result,opts) ;
-   setTrailerUrl(result);
+  const handleClick = (movie) => {
+    if (trailerUrl !== "") {
+      setTrailerUrl("");
+    } else {
+      var trail = movieTrailer(movie?.name || movie?.title);
+      // console.log(movie.name);
+      trail.then((result) => {
+        //console.log(result);
+        var youtubeId = getYoutubeId(result, opts);
+        setTrailerUrl(result);
+      });
+    }
+  };
+  var player;
+  function onYouTubeIframeAPIReady() {
+    console.log("done");
+    player = new YouTube.Player("player", {
+      height: "390",
+      width: "640",
+      videoId: trailerUrl,
+      events: {
+        onReady: onPlayerReady,
+      },
+    });
+  }
+  function onPlayerReady(event) {
+    event.target.playVideo();
+  }
+  console.log(trailerUrl);
 
-  })
-}
-   
-    //console.log(movie);
-   
-    // trail.then( url =>{
-    //   const urlParams = new URLSearchParams( new URL(url).search);
-    //     setTrailerUrl(urlParams.get('v'));
-    // })
-    
-      //  movieTrailer(movie?.title || "")
-      //  .then(url => {
-      //    const urlParams = new URLSearchParams( new URL(url).search);
-      //    setTrailerUrl(urlParams.get('v'));
-       
+  return (
+    <div className="row">
+      <h2>{title}</h2>
 
-      //  }).catch(error => console.log(error))
-      
-     }
-     var player;
-      function onYouTubeIframeAPIReady() {
-        console.log("done");
-        player = new YouTube.Player('player', {
-          height: '390',
-          width: '640',
-          videoId: trailerUrl,
-          events: {
-            'onReady': onPlayerReady,
-            
-          }
-        });
-      }
-      function onPlayerReady(event) {
-        event.target.playVideo();
-      }
-     console.log(trailerUrl);
+      <div
+        className="row__posters"
+        //     onMouseEnter={() => setIsHovered(true)}
+        // onMouseLeave={() => setIsHovered(false)} >
+      >
+        {movies?.map((movie) => (
+          <img
+            key={movie.id}
+            onClick={() => handleClick(movie)}
+            className={`row__poster  ${isLargeRow && "row__posterLarge"} `}
+            src={`${baseUrl}${
+              isLargeRow ? movie?.poster_path : movie?.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+        {/* {isHovered && (
+                  <>
+                   <video src="video/Start-Up.mp4" autoPlay={true} loop />
+                   
+                   </>
+              )} */}
+      </div>
+      {/* { trailerUrl && <ReactPlayer url={trailerUrl} width="100%" height="390px" controls={true}  playing={true} /> } */}
 
-    
-    return (
-        <div className="row" >
-            <h2>{title}</h2>
-
-            <div className="row__posters" >
-
-              {movies?.map(movie =>(
-                <img 
-                key={movie.id}
-                onClick={()=> handleClick(movie)}
-                className={`row__poster  ${isLargeRow && "row__posterLarge"} `}
-                src={`${baseUrl}${isLargeRow ? movie?.poster_path : movie?.backdrop_path}`}
-                alt={movie.name} 
-                
-                />
-              ))}
-
-            </div>
-            { trailerUrl && <ReactPlayer url={trailerUrl} width="100%" height="390px" controls={true}  playing={true} /> }
-       
-             {/* {trailerUrl && < YouTube videoId={trailerUrl} opts={opts} />} */}
-            
-            
-        </div>
-        
-    )
+      {/* {trailerUrl && < YouTube videoId={trailerUrl} opts={opts} />} */}
+    </div>
+  );
 }
 
 export default Row;
